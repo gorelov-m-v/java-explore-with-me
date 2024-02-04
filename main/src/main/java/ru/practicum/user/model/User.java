0ca9f4c9141b01c1ec230f65a2e71528d394c468
gorelov-m-v <mixer880@yandex.ru>
2024-02-04 12:47:18 +0300

    feat-реализовал AdminUser для main-service(2)

--- main/src/main/java/ru/practicum/user/model/User.java
+++ main/src/main/java/ru/practicum/user/model/User.java
@@ -1,11 +1,26 @@
 package ru.practicum.user.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Objects;
 
+@Entity
+@Table(name = "users", schema = "public")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

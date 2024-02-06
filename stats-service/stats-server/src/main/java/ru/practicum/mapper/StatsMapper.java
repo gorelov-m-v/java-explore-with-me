5e37455d1b9da204ca828741a54bcf8a3b225b47
gorelov-m-v <mixer880@yandex.ru>
2024-02-06 19:47:11 +0300

    fix-фикс после второго ревью

--- stats-service/stats-server/src/main/java/ru/practicum/mapper/StatsMapper.java
+++ stats-service/stats-server/src/main/java/ru/practicum/mapper/StatsMapper.java
@@ -10,12 +10,14 @@ public class StatsMapper {
 
     public static Endpoint toEndpoint(HitDto hitDto) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
         Endpoint endpoint = new Endpoint();
-        endpoint.setIp(hitDto.getIp());
-        endpoint.setUri(hitDto.getUri());
-        endpoint.setApp(hitDto.getApp());
-        endpoint.setDateTime(LocalDateTime.parse(hitDto.getTimestamp(), formatter));
+        
+        if (hitDto != null) {
+            endpoint.setIp(hitDto.getIp());
+            endpoint.setUri(hitDto.getUri());
+            endpoint.setApp(hitDto.getApp());
+            endpoint.setDateTime(LocalDateTime.parse(hitDto.getTimestamp(), formatter));
+        }
 
         return endpoint;
     }

715736daa236f4e3db223dea60ca83dd4fbe0a36
gorelov-m-v <mixer880@yandex.ru>
2024-02-11 03:38:31 +0300

    fix-кодстайл

--- main-service/src/main/java/ru/practicum/main/comment/model/Comment.java
+++ main-service/src/main/java/ru/practicum/main/comment/model/Comment.java
@@ -2,16 +2,11 @@ package ru.practicum.main.comment.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import ru.practicum.main.event.model.Event;
-import ru.practicum.main.user.model.User;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Setter
--- main-service/src/main/java/ru/practicum/main/comment/repository/CommentsRepository.java
+++ main-service/src/main/java/ru/practicum/main/comment/repository/CommentsRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.main.comment.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.practicum.main.comment.model.Comment;
 import java.util.List;
